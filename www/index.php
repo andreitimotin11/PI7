42441e97863fe3aabde91faa76bb08b6ca418ae8
Andrei Timotin <andreitimotin11@gmail.com>
2017-07-29 16:11:32 +0300

    test

--- www/index.php
+++ www/index.php
@@ -5,4 +5,11 @@
  * Date: 29.07.2017
  * Time: 15:50
  */
-echo phpinfo();
+$dsn = 'mysql:host=127.0.0.1;dbname=test';
+$dbh = new PDO($dsn, 'root', '');
+//var_dump($dbh);
+$sth = $dbh->prepare('SELECT * FROM persons');
+$sth->execute();
+
+$data = $sth->fetchAll();
+var_dump($data);
